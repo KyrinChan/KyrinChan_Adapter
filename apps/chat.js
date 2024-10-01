2d24366475f288829712c69eab3d3e6923c11db4
K2Lin-Daniel <77517478+K2Lin-Daniel@users.noreply.github.com>
2024-10-02 00:56:11 +0800

    debug

--- apps/chat.js
+++ apps/chat.js
@@ -1327,6 +1327,7 @@ export class chatgpt extends plugin {
             debug: Config.debug
           })
           // 档案前馈
+          let userjson;
           if (Config.Gen6Impressions) {
             const dir = 'resources/KyrinChanGEN6/impressions/data'
             const filename = `${e.sender.user_id}.json`
@@ -1349,7 +1350,7 @@ export class chatgpt extends plugin {
           }
           let msg = '请根据这段设定为这段对话生成一个符合凯琳酱设定且自然的回复："' + Config.standaloneGen6Settings + '"，和你对话的人是"' + userjson + '"，对话上文是"' + prompt + '" 要尽可能地自然而有趣。'
           let res = await client.sendMessage(msg, "")
-          logger.info(`GEN6特殊回复成功: ${res.text}`)
+          logger.info(`GEN6特殊回复成功: ${res.text} ${userjson}`)
           response = res.text;
           await this.renderImage(e, use, response, prompt, "", "", "", "", "")
           // 印象功能
@@ -1449,6 +1450,7 @@ export class chatgpt extends plugin {
           debug: Config.debug
         })
         // 档案前馈
+        let userjson;
         if (Config.Gen6Impressions) {
           const dir = 'resources/KyrinChanGEN6/impressions/data'
           const filename = `${e.sender.user_id}.json`
@@ -1471,7 +1473,7 @@ export class chatgpt extends plugin {
         }
         let msg = '以下是一段对话的回复，"' + response + '" ，请将它变得更加风格化，更符合设定且更加自然，具体设定为"' + Config.enhanceGen6Settings + '"，和你对话的人是"' + userjson + '"，同时依据对话上文"' + prompt + '"适当进行修改，使其更加符合凯琳酱的设定。仅输出修改后的回复。'
         let res = await client.sendMessage(msg, "")
-        logger.info(`增强回复成功: ${response}`)
+        logger.info(`增强回复成功: ${response} ${userjson}`)
         response = res.text;
         // 印象功能
         if (Config.Gen6Impressions) {
