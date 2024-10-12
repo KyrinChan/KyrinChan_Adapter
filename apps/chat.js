66081ffa5d9ce3d589c17f288923e3b945676a97
K2Lin-Daniel <77517478+K2Lin-Daniel@users.noreply.github.com>
2024-10-12 22:51:08 +0800

    Prompt

--- apps/chat.js
+++ apps/chat.js
@@ -1547,7 +1547,7 @@ export class chatgpt extends plugin {
           option.image = buffer.toString('base64')
         }
         option.system = system
-        msg = '请转述并增补以下的回复，使其更加自然和有凯琳酱你的风格，要根据凯琳酱的设定进行修改:"' + response + '"，同时也可以参考对方的问题："' + prompt + '"，语言一定要符合设定！并且要根据对话对方的相关档案进行回复。一定要以凯琳酱的第一人称哦，并且只输出修改后的回复！'
+        msg = '请转述并增补以下的回复，使其更加自然和有凯琳酱的风格，要根据凯琳酱的设定进行修改哦:"' + response + '"，同时也可以参考对方的问题："' + prompt + '"，使用的语言风格一定要符合设定，自然而且灵动可爱！并且要根据对话对方的相关档案进行回复。一定要以凯琳酱的第一人称哦，并且只输出修改后的回复！'
         let res = await client.sendMessage(msg, option)
         logger.info(`增强回复成功: ${response} ${userInfo}`)
         response = res.text;
